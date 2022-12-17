b8501beafd56efa1939f9faaabf19a920d3f7e52
Aimar3007 <88170348+Aimar3007@users.noreply.github.com>
2022-12-17 12:28:51 +0700

    Update mysql.js

--- src/config/db/mysql.js
+++ src/config/db/mysql.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize')
-var db = new Sequelize('katalog_hp', 'root', '', {
+var db = new Sequelize('katalog_hp', 'root', 'password', {
     dialect: 'mysql',
     host: 'localhost',
     operatorAliases: false
